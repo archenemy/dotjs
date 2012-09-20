5161ea946d1f679937d97ac0837f75523a72c042
Fernando G <fgomez@gmail.com>
2012-09-20 08:59:28 +0200

    un par de filtrillos más

--- zavvi.es.js
+++ zavvi.es.js
@@ -7,11 +7,13 @@ if ( (document.location.pathname.indexOf('mega-lunes') != -1 && document.locatio
 	$('.item-games-games-accessories').hide();
 	$('.item-games-platforms-pc').hide();
 	$('.item-games-platforms-nintendo-wii').hide();
+	$('.item-games-platforms-wii-u').hide();
 	$('.item-games-platforms-ps-vita').hide();
 	$('.item-games-platforms-nintendo-ds').hide();
 	$('.item-games-platforms-psp').hide();
 	$('.item-games-platforms-nintendo-3ds').hide();
 	$('.item-games-platforms-ps-vita-ps-vita-accessories').hide();
 	$('.item-games-consoles').hide();
+	$('.item-games').hide();
 	$('.panel-content').prepend('<div style="font-size: 120%; width:95%; background: yellow; padding: 1em; text-align: center;">Ojo, ocultando via .js todo lo que no es xbox</div>');
 }
